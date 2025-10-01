6ba99d6e8b88a0f43a19bc8bc4794414fa70616d
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-01 13:39:20 +0100

    forEach to create homepage entries

--- client/src/homepage.js
+++ client/src/homepage.js
@@ -1,12 +1,32 @@
-//const availableBooks = document.getElementById("available-books")
+const availableBooks = document.getElementById("available-books");
 
 //get details from database using async fetchBooks() with book-details and calls below function
+async function fetchBooks() {
+  const response = await fetch("http://localhost:8080/book-details");
+  const books = await response.json();
+  createBooks(books);
+}
 
-//create forEach(book) function that creates elements based on database
-//  create a div and then create an element for each title, author and img. set
-//  set .innerText = book.title for all components
-//  give the div a class attribute to css
-//  append elements to div
-//  append div to availableBooks
+function createBooks(booksArray) {
+  booksArray.forEach((book) => {
+    const div = document.createElement("div");
 
-//don't forget to call the fetchBooks() function!
+    const titleElement = document.createElement("p");
+    const authorElement = document.createElement("p");
+    const imgElement = document.createElement("img");
+
+    titleElement.innerText = book.title;
+    authorElement.innerText = book.author;
+    imgElement.src = book.cover_url;
+
+    div.setAttribute("class", "listOfBooks");
+
+    div.append(titleElement, authorElement, imgElement);
+
+    availableBooks.append(div);
+  });
+}
+
+fetchBooks();
+
+//do you use a click event to navigate to a new webpage?
